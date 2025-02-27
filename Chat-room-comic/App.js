a39f6d67ac3d63171703921d612773cf27b77ed8
Kyle <email@kylenoad.com>
2025-02-27 16:33:29 +0000

    adding auth login

--- Chat-room-comic/App.js
+++ Chat-room-comic/App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>WELCOME TO OUR APP</Text>
+      <Text>WELCOME</Text>
       <StatusBar style="auto" />
     </View>
   );
