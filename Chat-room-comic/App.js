801825e049dfa2f4a8877061ad95258ef4f40c39
Tommy Christ M1 Pro <tommychristm1pro@tommys-mbp.home>
2025-02-28 09:41:32 +0000

    App.js/updated

--- Chat-room-comic/App.js
+++ Chat-room-comic/App.js
@@ -1,17 +1,39 @@
 import app from "./firebaseConfig";
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import auth from "@react-native-firebase/auth";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
+const Stack = createStackNavigator();
 
 export default function App() {
+  const [initializing, setInitializing] = React.useState(true);
+  const [user, setUser] = React.useState(null);
+  function onAuthStateChanged(user) {
+    setUser(user);
+    if (initializing) setInitializing(false);
+  }
+  React.useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber;
+  }, []);
+  if (initializing) return null;
   return (
-    <View style={styles.container}>
-      <Text>WELCOME</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {user ? (
+          <Stack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <Stack.Screen
+            name="Login"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
